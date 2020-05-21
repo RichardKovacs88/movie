cd4aa44e8921c512da7bd3e8e0eef9f4ad9d9da6
Kovács Richárd <gismo@Kovacs-MacBook-Pro.local>
2020-05-21 22:50:07 +0200

    1

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,109 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import axios from 'axios';
+import Search from './components/Search';
+import Results from './components/Results';
+import Popup from './components/Popup';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+
+import { IResults } from './components/IResults';
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
 
 function App() {
+ 
+  const [state, setState] = useState({
+    s: "",
+    results: [],
+    selected: {},
+  });
+
+  const apiurl = "http://www.omdbapi.com/?apikey=4a3b711b";
+
+  const search = (e: any) => {
+    if (e.key === "Enter") {
+      axios(apiurl + "&s=" + state.s).then(({ data }) => {
+        
+        let results = data.Search;
+        if(results){
+        setState(prevState => {
+          return { ...prevState, results: results }
+        })}
+      });
+    }
+  }
+  
+  const handleInput = (e: any) => {
+    let s = e.target.value;
+
+    setState(prevState => {
+      return { ...prevState, s: s }
+    });
+  }
+
+  const openPopup = (id: string) => {
+    axios(apiurl + "&i=" + id).then(({ data }) => {
+      let result = data;
+
+      console.log(result);
+
+      setState(prevState => {
+        return { ...prevState, selected: result }
+      });
+    });
+  }
+
+  const closePopup = () => {
+    setState(prevState => {
+      return { ...prevState, selected: {} }
+    });
+  }
+  const classes = useStyles();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <React.Fragment>
+   <div className="App">
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            Movie Database
+          </Typography>
+          
+        </Toolbar>
+      </AppBar>
     </div>
+      <main>
+        <Search handleInput={handleInput} search={search} />
+        
+        <Results results={state.results} openPopup={openPopup} />
+
+        {(typeof state.selected.Title != "undefined") ? <Popup selected={state.selected} closePopup={closePopup} /> : false}
+      </main>
+    </div>
+   
+    </React.Fragment>
   );
 }
 
-export default App;
+export default App
